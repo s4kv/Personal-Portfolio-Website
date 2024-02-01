6461644fd798c8a07e33d1558492d698ef3069c2
salvador <71200477+s4kv@users.noreply.github.com>
2024-02-01 17:33:17 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,107 @@
+# This File will contain the information to be displayed in your portfolio
+
+# CHANGE BELOW
+profile_picture = "images/profile.jpeg"
+about_me = "I'm Salvador Ibarguren. Student of Computer Science who loves playing with code and tech!"
+
+
+# CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+# CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/salvador-ibarguren/"
+my_github_url = "https://github.com/s4kv"
+my_email_address = "smatto3@gatech.edu"
+
+
+education_data = {
+    "Degree": "Bachelor of Science in Computer Science",
+    "Institution": "Georgia Institute of Technology",
+    "Location": "Atlanta, GA",
+    "Graduation Date": "2027",
+    "GPA": "4.0",
+}
+course_data = {
+    "code": ["CS 1301", "APPH 1040", "MATH 1551", "HIST 2112"],
+    "names": [
+        "Intro to CS",
+        "Scientific Foundations of Health",
+        "Differential Calculus",
+        "The United States since 1877",
+    ],
+    "semester_taken": ["1st", "1st", "1st", "1st"],
+    "skills": [
+        "Baby steps with Python!",
+        "Let's get healthy",
+        "The start of the Cacl journey",
+        "We learn about history!",
+    ],
+}
+experience_data = {
+    "Computer Science Teacher": (
+        [
+            "- Voluntary teacher at San Pedro School of 5th and 6th year students.",
+            "- Taught 31 students programming to produce their own video games using the platform Scratch.",
+            "- Designed and prepared a class chronogram and materials for a full year of the class.",
+        ],
+        "images/teacher.jpg",
+    ),
+    "MIT’A": (
+        [
+            "- Leader of the technology department.",
+            "- Implementation of a real-time database with information from the Geophysical Institute of Peru using JavaScript and Python for web scrapping.",
+            "- Responsible for the maintenance and development of the website.",
+        ],
+        "images/seismo.jpg",
+    ),
+    "RoboWrestling": (
+        [
+            "- Member of the RoboWrestling club of RoboJackets in Georiga Institute of Technology."
+        ],
+        "images/robor.png",
+    ),
+}
+
+projects_data = {
+    "Surveillance Camera | Visual Recognition": "Developed a movement-responsive camera using python, Haar Cascades and artificial intelligence.",
+    "Automated Zoom Scheduler | Macro Automatization": "Developed a bash and python macro that auto-joins a list of Zoom Meetings on a time and date.",
+    "WhatsApp Bot | Bot Automatization": "Developed a WhatsApp bot that sends pre-written messages at specific times.",
+    "Nobel Finder | Web Scrapping & Web Application": "Developed a web scrapper and web application that finds information about Nobel Prize Winners.",
+}
+
+programming_data = {"Python": 90, "Java": 65, "C++": 30, "Rust": 40}
+
+# CHANGE BELOW (OPTIONAL)
+programming_icons = {"Python": "🐍", "Java": "☕", "C++": "🔍", "Rust": "🦀"}
+spoken_icons = {"French": "🇫🇷", "English": "🇬🇧", "Spanish": "🇪"}
+
+# CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Intermediate Level",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "P'ukuro NGO | Co-Founder": (
+        [
+            "- Aided and empowered 150 families and ensured more than 450 daily food rations by building safe cooking structures, giving educational talks, and sharing informative content.",
+            "- Designed and implemented eco-sustainable wood stoves to support low-income communities",
+        ],
+        "images/pukuro.png",
+    ),
+    "Yaku NGO | Co-Founder": (
+        [
+            "- Designed and built a highly efficient water condenser and filter that uses fog nets and the Peltier effect for Peruvian communities with no easy access to water.",
+            "- Presented talks and educational workshops organized by us and the Municipality of Lima.",
+        ],
+        "images/yaku.png",
+    ),
+}
+activity_data = {
+    "Codecademy Course": [
+        "- Codeacademy: Learn Python",
+        "- Codecademy: Learn Intermediate Python",
+    ]
+}
